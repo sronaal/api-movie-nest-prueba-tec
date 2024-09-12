eaf04dc3b0ddf8941347175577254fbc00511503
sronaal <ronal.sanx21@gmail.com>
2024-09-12 16:22:39 -0500

    Integracion Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
 
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ValidationPipe } from "@nestjs/common";
+
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  
+
+  app.enableCors({origin:'*'})
+  app.setGlobalPrefix('/api/v1')
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform:true
+    })
+  )
+
+  const config = new DocumentBuilder()
+  .setTitle("API Prueba Tecnica Movies")
+  .setVersion('1.0')
+  .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('docs', app, document)
+
+  await app.listen(3100);
 }
 void bootstrap();
