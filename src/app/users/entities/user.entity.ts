428beacd6abd181c9f7a01035057b19ec0f8e0a6
sronaal <ronal.sanx21@gmail.com>
2024-09-12 17:19:10 -0500

    Edicion tsconfig / Creacion entidad User

--- src/app/users/entities/user.entity.ts
+++ src/app/users/entities/user.entity.ts
@@ -1,1 +1,33 @@
-export class User {}
+import { hash } from "crypto";
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { v4 as uuid} from 'uuid'
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id!: number ;
+
+
+    @Column()
+    nombre_completo: string;
+
+    @Column()
+    usuario: string;
+
+    @Column()
+    correo: string;
+
+    @Column()
+    password: string;
+
+
+    @BeforeInsert()
+    hashPassword(){
+        let hashPassword = hash(this.password,2)
+        this.password = hashPassword
+    }
+
+}
+
+
